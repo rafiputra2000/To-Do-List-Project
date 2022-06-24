1e2b52c4dc3438ef2ad2babc1fcfd90e693882e6
Rafi <rafi.putra2000@gmail.com>
2022-06-24 21:48:48 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy food", "Cook food", "Eat food"];
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', function(req, res){
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
+    let day = today.toLocaleDateString("id-ID", options);
+
+    // let currentToday = today.getDay();
+    // let day = "";
+
+    // // res.send("this day " + currentToday);
+    // switch (currentToday) {
+    //     case 0:
+    //         day="Minggu"
+    //         break;
+    //     case 1:
+    //         day="Senin"
+    //         break;
+    //     case 2:
+    //         day="Selasa"
+    //         break;
+    //     case 3:
+    //         day="Rabu"
+    //         break;
+    //     case 4:
+    //         day="Kamis"
+    //         break;
+    //     case 5:
+    //         day="Jumat"
+    //         break;
+    //     case 6:
+    //         day="Sabtu"
+    //         break;
+    
+    //     default:
+    //         console.log("Error: This current day is equal to " + currentToday)
+    //         break;
+    // }
+
+    // if(currentToday === 6 || currentToday === 0){
+    //     // res.send("yeay this a weekend");
+    //     day ="weekend";
+    // } else {
+    //     // res.send("This day for work");
+    //     day="weekday"
+    // }
+    res.render("list", {
+        kindOfDay: day,
+        listNewItem: items
+    
+    });
+    
+});
+
+app.post('/', function (req,res){
+    let item = req.body.listItem;
+
+    items.push(item);
+
+    res.redirect('/');
+
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});

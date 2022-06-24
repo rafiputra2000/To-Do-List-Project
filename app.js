f5e41210510218f94ba45f5babc178c5a1030b5f
Rafi <rafi.putra2000@gmail.com>
2022-06-24 22:08:34 +0700

    add index.html

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let items = ["Buy food", "Cook food", "Eat food"];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
     let today = new Date();
